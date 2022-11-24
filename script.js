9bb6cd3cb7bedd4906a5f01d7b5868afa4ed0356
Joakim <joakim.andersson920202@gmail.com>
2022-11-24 10:32:51 +0100

    Add module for Gameflow and Gameboard, add factory function for Player

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hej");
+const GameBoard  = function () {
+
+    gameBoard = [1,2,3,4,5,6,7,8,9];
+
+    return gameBoard;
+}();
+
+const Player = function (score, isAI) {
+    let playerScore = score;
+    let human = isAi;
+
+    let chooseSquare = (choice) => {
+
+    }
+}
+
+const GameFlow = function () {
+    
+}();
+
