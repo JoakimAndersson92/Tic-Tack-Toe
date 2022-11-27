32660c8b575294e36d082c4dcdd8d0b6467f3892
Joakim <joakim.andersson920202@gmail.com>
2022-11-27 16:46:47 +0100

    Switch player turn between choice

--- script.js
+++ script.js
@@ -16,29 +16,35 @@ const GameBoard = function () {
 }();
 
 
-const Player = function (marker) {
+const Player = function (marker, turn) {
     let playerScore = 0;
     let playerMarker = marker;
+    let playerTurn = turn;
 
     let chooseSquare = function ()  {
         if(typeof marker === 'object') return;
-        this.textContent = marker;
+        if(playerTurn){
+            this.textContent = marker;
+        }
+        playerTurn = !playerTurn;
     }
 
     Querys.squares.forEach(element => {
-        element.addEventListener('click', chooseSquare);
+        element.addEventListener('click', chooseSquare, {once : true});
     });
 
     return{
         playerScore,
         playerMarker,
+        playerTurn,
     }
 }
 
-const newPlayer  = Player("X");
 
 const GameFlow = function () {
 
+    const firstPlayer  = Player("X", true);
+    const secondPlayer  = Player("O", false);    
      
 }();
 
